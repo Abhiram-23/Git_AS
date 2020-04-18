0ffbf1a563efe4fb761b1f7e9e87386939989144
Abhiram-23 <abhi_ram2000@users.noreply.github.com>
2020-04-18 21:34:45 +0530

    main activity and vcs xml file commited ;added

--- app/src/main/java/com/example/git_as/MainActivity.java
+++ app/src/main/java/com/example/git_as/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("abho","ajsdfj")
+        Log.i("abho","ajsdfj");
     }
 }
